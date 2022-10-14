1356eaf31d25fa0e57e66354e4938c00ae447c14
Ks14021999 <ks14021999@gmail.com>
2022-10-15 01:28:05 +0530

    first commit

--- config/index.ts
+++ config/index.ts
@@ -1,4 +1,4 @@
-export const MONGO_URI = 'mongodb+srv://ks14:qazwsxedc@mern1.xq4bs.mongodb.net/food?retryWrites=true&w=majority'
+export const MONGO_URI = 'mongodb address'
 export const APP_SECRET = '238745623hsdf'
 
 export const PORT = 5000;
