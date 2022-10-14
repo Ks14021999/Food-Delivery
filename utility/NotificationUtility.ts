03d5d3b9334d50f2d6b69421573394e8b1837adb
Ks14021999 <104979140+Ks14021999@users.noreply.github.com>
2022-10-15 02:55:01 +0530

    Update NotificationUtility.ts

--- utility/NotificationUtility.ts
+++ utility/NotificationUtility.ts
@@ -11,8 +11,8 @@ export const GenerateOtp = () => {
 
 export const onRequestOTP = async(otp: number, toPhoneNumber: string) => {
 
-    const accountSid = "ACab1e91b82e0c3e9c88bc8a2da4856d96";
-    const authToken = "8614e2e33d40210fe2ab20740b01a899";
+    const accountSid = "accountSid";
+    const authToken = "authtoken";
     const client = twilio(accountSid, authToken);
 
     const response = await client.messages.create({
